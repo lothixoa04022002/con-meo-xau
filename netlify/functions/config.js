f92b1b391b37d1656f5bc819c876a9e8593f8b92
lothixoa04022002 <lothixoa04022002@gmail.com>
2025-10-10 19:19:27 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,7 +1,7 @@
 const config = {
-    token: '7963473156:AAFUqFzx6nsiEK0GycwzSLTaKvKjaVjkpK4',
+    token: '8303564128:AAFx6X7N-CuUv8oZgm9T7utJ6IfYWaWsG10',
     chat_id: '-6604062347',
-    noti_token: '7963473156:AAFUqFzx6nsiEK0GycwzSLTaKvKjaVjkpK4',
+    noti_token: '8303564128:AAFx6X7N-CuUv8oZgm9T7utJ6IfYWaWsG10',
     noti_chat_id: '-4983846411',
 };
 
